d5cfc0a047cf49f811c68b7da78b7a0dfddfa8ed
andreimihailescu <andrei.mihailescu994@gmail.com>
2017-09-30 21:16:02 +0300

    Add Inheritance

---
+++ Inheritance/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack();
+            stack.Push("Andrei");
+            stack.Push(2);
+            stack.Push(5.5f);
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+        }
+    }
+}
---
+++ Inheritance/Stack.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Inheritance
+{
+    public class Stack
+    {
+        private readonly List<object> _list = new List<object>();
+
+        public void Push(object obj)
+        {
+            if (obj == null)
+            {
+                throw new InvalidOperationException("CUSTOM MADE!!!");
+            }
+
+            _list.Add(obj);
+        }
+
+        public object Pop()
+        {
+            var popElement = _list.Last();
+
+            //Console.WriteLine(popElement);
+
+            _list.Remove(_list.Last());
+
+            return popElement;
+        }
+
+        public void Clear()
+        {
+            _list.Clear();
+        }
+    }
+}
