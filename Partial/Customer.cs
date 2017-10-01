5c1708659f4c0e0e88121e4b010ca5d56cca1370
andreimihailescu <andrei.mihailescu994@gmail.com>
2017-10-01 12:11:22 +0300

    Add Partial

---
+++ Partial/Customer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Partial
+{
+    public partial class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public void GetDetails()
+        {
+            Console.WriteLine("Hi! My name is {0} {1} and i am {2} years old.", FirstName, LastName, Age);
+        }
+    }
+
+    public partial class Person
+    {
+        public bool Subscribed { get; set; }
+
+        public void GetDetails(string hobby)
+        {
+            Console.WriteLine("Hi! My name is {0} {1} and i am {2} years old and my hobby is {3}", FirstName, LastName, Age, hobby);
+        }
+    }
+}
---
+++ Partial/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Partial
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var newCustomer = new Person()
+            {
+                FirstName = "Andrei",
+                LastName = "Mihailescu",
+                Age = 22
+            };
+
+            newCustomer.GetDetails();
+            newCustomer.GetDetails("Basketball");
+        }
+    }
+
+
+}
